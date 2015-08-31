bd39562cdd0b79e0e2977ba452513d7a37e89804
dlunny <dlunny@featurecreeptechnology.com>
2015-08-31 12:09:33 -0400

    fixes a 'done() not defined' error

--- generators/export/index.js
+++ generators/export/index.js
@@ -19,6 +19,8 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   prompting: function () {
+    var done = this.async();
+
     if (!this.name) {
       prompts.askName.apply(this, [this.type, function(answer) {
         this.name = answer;
